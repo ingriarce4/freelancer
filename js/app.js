1b6ae232ce6e9a9b0ed25649d4ad62694199f462
ingriarce4 <ingridarce4@gmail.com>
2017-06-01 21:57:34 -0400

    avances en html

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
 /*
  * Archivo principal de JS
  */
+
+ (function main(){
+	var boxes = Array.from(document.getElementsByClassName("imagenes"));
+	var portafolio = document.getElementById("portafolio");
+
+	var modal, bodyModal, close, img;
+	boxes.forEach(function(box){
+		box.addEventListener("click", function(){
+			modal = document.createElement("div");
+			modal.classList.add("box-servicios-modal");
+			bodyModal = document.createElement("div");
+			bodyModal.classList.add("modal-body");
+			bodyModal.innerHTML = box.innerHTML;			
+			modal.appendChild(bodyModal);
+			
+			close = document.createElement("div");
+			close.classList.add("close");
+			img = document.createElement("img");
+			img.setAttribute("src", "https://d30y9cdsu7xlg0.cloudfront.net/png/196863-200.png");
+			close.appendChild(img);
+			modal.appendChild(close);
+			portafolio.appendChild(modal);
+			close.addEventListener("click",function(){
+				modal.classList.add("hide");
+				portafolio.removeChild(modal);
+			});
+		});		
+	});
+})();
